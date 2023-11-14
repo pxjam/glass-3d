22f64fccfd5bf2ea2404f97fac9f53cd92fa528e
Dmitry Orlov <dmitryorly611@gmail.com>
2023-11-14 16:28:00 +0300

    feat: tune scene

--- src/App.js
+++ src/App.js
@@ -24,8 +24,8 @@ export const App = () => {
 
     const scene = new Scene()
 
-    const camera = new PerspectiveCamera(45, width / height, 1, 1000)
-    camera.position.set(4, 2, 10)
+    const camera = new PerspectiveCamera(45, width / height, 1, 20)
+    camera.position.set(2, 1, 2)
 
     const bgGeometry = new PlaneGeometry(1, 1)
     const video = document.querySelector('.video')
@@ -35,11 +35,11 @@ export const App = () => {
     const bgMesh = new Mesh(bgGeometry, bgMaterial)
 
     camera.add(bgMesh)
-    bgMesh.position.set(0, 0, -200)
+    bgMesh.position.set(0, 0, -10)
     scene.add(camera)
 
     const resizeBackground = () => {
-        const unitRatio = unitsPerPixelAtDepth(camera, camera.position.z - bgMesh.position.z)
+        const unitRatio = unitsPerPixelAtDepth(camera, Math.abs(bgMesh.position.z))
         const x = video.offsetWidth * unitRatio
         const y = video.offsetHeight * unitRatio
         bgMesh.scale.set(x, y, 1)
@@ -47,17 +47,13 @@ export const App = () => {
     }
     resizeBackground()
 
-
-
     const can = createCan()
-    can.position.z = 5
     scene.add(can)
     attachPane(can)
 
     const light = new DirectionalLight(0xfff0dd, 1)
-    light.position.set(0, 5, 10)
-    scene.add(light)
-
+    light.position.set(0, 2, -5)
+    camera.add(light)
 
     const controls = new OrbitControls(camera, renderer.domElement)
     controls.target = can.position
